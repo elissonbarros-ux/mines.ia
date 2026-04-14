ef81f87e0c17112a9b9bc95419d06e5b5d95f564
elissonbarros-ux <elisson.barros@gmail.com>
2026-04-13 22:45:59 -0300

    fix render porta certo

--- app.py
+++ app.py
@@ -67,5 +67,8 @@ def play():
     treinar()
     return jsonify(prever())
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
